5c3c6f4e31bb88a5f705aad532174b4ea1e84f24
phunq71 <ngoquocphu71@gmail.com>
2025-05-07 13:00:58 +0700

    Update CallpackController.java

--- src/main/java/com/demo/controller/CallpackController.java
+++ src/main/java/com/demo/controller/CallpackController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.demo.dao.OrderDAO;
 import com.demo.dto.TransactionDTO;
 import com.demo.entity.Order;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @CrossOrigin("*")
